8172fc9b7d7e534813fa61aa88533ebaea278e4f
Fira-Tefe <firaolteferi90@gmail.com>
2024-12-05 23:28:39 +0300

    fourth

--- index.js
+++ index.js
@@ -2,18 +2,30 @@
 let secret = document.querySelector('.secretbox').textContent;
 secret = Math.trunc((Math.random()*20)+1);
 console.log(secret, typeof secret);
-document.querySelector('.secretbox').textContent= secret;
+
 
 document.querySelector('.gbutton').addEventListener('click', function(){
     let guessnumber = Number(document.querySelector('#inputn').value);
  
     if(!guessnumber){
         document.querySelector('.guess').textContent = 'Wrong Input, Try again';
+        document.querySelector('.cmessage').textContent = 'Fail!';
     }
     else if(guessnumber > secret){
         document.querySelector('.guess').textContent = 'The number is too high';
+        document.querySelector('.cmessage').textContent = 'Fail!';
     }
     else if(guessnumber < secret){
         document.querySelector('.guess').textContent = 'The number is too low';
+           document.querySelector('.cmessage').textContent = 'Fail!';
     }
-})
+    else if(guessnumber == secret){
+        document.querySelector('.secretbox').textContent= secret;
+        document.querySelector('.guess').textContent = 'Correct guessing!'
+        document.querySelector('.cmessage').textContent = 'Congra!';
+        document.querySelector('.container').style.backgroundColor = 'blue'; 
+    }
+});
+document.querySelector('.button').addEventListener('click', function(){
+    location.reload();
+});
