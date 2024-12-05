bde74fec5ab857d0da34cb64b76c67c5eb9ce867
Fira-Tefe <firaolteferi90@gmail.com>
2024-12-05 22:46:36 +0300

    second

--- index.js
+++ index.js
@@ -6,5 +6,14 @@ document.querySelector('.secretbox').textContent= secret;
 
 document.querySelector('.gbutton').addEventListener('click', function(){
     let guessnumber = Number(document.querySelector('#inputn').value);
-    console.log(guessnumber ,typeof guessnumber);
+ 
+    if(!guessnumber){
+        document.querySelector('.guess').textContent = 'Wrong Input, Try again';
+    }
+    else if(guessnumber > secret){
+        document.querySelector('.guess').textContent = 'The number is too high';
+    }
+    else if(guessnumber < secret){
+        document.querySelector('.guess').textContent = 'The number is too low';
+    }
 })
